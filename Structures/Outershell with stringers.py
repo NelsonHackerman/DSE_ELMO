4c56b3e339ec30d95950f6e08d08cac5aa0564f1
VPathirana <127831994+VPathirana@users.noreply.github.com>
2024-12-05 15:08:10 +0100

    Cleaning up the code

--- Structures/Outershell with stringers.py
+++ Structures/Outershell with stringers.py
@@ -7,68 +7,35 @@ GREEN = "\033[92m"
 RED = "\033[91m"
 RESET = "\033[0m"
 
-#option 2: longitudinal stiffeners
-#material: AL 7075
-# mat=1
-# if mat==1:
-#     E=71*10**9#N/m2
-#     v=0.33
-#     rho=2.8*10**3#kg/m3
-#     Sig_tu=524*10**6#N/m2 ultimate tensile strength
-#     Sig_ty=448*10**6#N/m2 yield tensile strength
-# if mat==2:
-#     E=110*10**9
-#     v=0.31
-#     rho=4.43*10**3
-#     Sig_tu=900*10**6
-#     Sig_ty=855*10**6
-# g=9.81
-# SF=1.6 #table 11-54 SMAD
-#falcon 9: axial -2 to 6g, long -2 to 2g + for compress
-#arianes: axial -6 to 2.5g, long -1.8 to 1.8g - for compress
-#overall axial -6 to 2.5g, long -2 to 2g (I'M USING + FOR TENSION)
-#falcon 9: axial >25Hz, lat >10Hz
-#arianes: axial >20Hz, lat >6Hz
-#overall axial >25Hz, lat >10Hz
+#OPTION 2: shell with longitudinal stiffeners
 
 
-p=0#pressure for tanks
 
-
-#sizing for natural frequency
-
-#l=np.sqrt(E/rho)*0.25/f_ax #equation for axial natural frequency
-#print('Required length for natural frequency monocoque : ',l)
-A=f_ax**2*m*l/(E*0.25**2)
-
-t1=A/(np.pi*d)
+#sizing for axial natural frequency
+A=f_ax**2*m*l/(E*0.25**2) #Calculating minimum area
+t1=A/(np.pi*d) #Calculating minimum thickness from area
 print('Estimated thickness ax vibration: ',t1*1000,' mm')
 
 
 
-#d=f_lat*l**2/0.56*np.sqrt(8*rho/E)#equation for lateral natural frequency
-I_req=f_lat*m*l**3/(0.56**2*E)
+#Sizing for longitudinal natural frequency
+I_req=f_lat**2*m*l**3/(0.56**2*E) #minimum MOI
+
 print('Required MOI ',I_req)
-t2=I_req/(np.pi*r**3)
+t2=I_req/(np.pi*r**3) #minimum thickness for MOI, Im ignoring the stiffeners rn because the skin can withstand this alone
 print('Estimated thickness lat vibration(only skin): ',t2*1000,' mm')
 
 
-
-
-#equivalent axial tensile load with bending
-
-
-#Sizing for tensile strength 
+#Sizing for tensile strength with bending moment included
 t3=l_eq_ten*SF/(np.pi*d*Sig_ty)
 print('Estimated thickness tension mode: ',t3*1000,' mm')
 
 #thickness t1 is the most limiting right now so I will be using that
 t=max(t1,t2,t3)
-t=0.001
-
-#Sizing for compressive strength
 
 
+#Sizing for compressive buckling strength with bending moment included
+t=0.001 #Manually changing the thickness here to meet buckling req
 A=np.pi*d*t
 
 Req_buck=l_eq_com/A 
@@ -76,23 +43,36 @@ n=200 #number of stringers
 b=np.pi*d/n #distance between stringers
 ts=0.001 #thickness of 1 stringer
 ls=0.02 # total length of 1 stringer
-val1=b**2/r/t*np.sqrt(1-v**2)
-print(f'Read off table, r/t: {GREEN}{r/t}{RESET}, x axis: {GREEN}{val1}{RESET}')
+val1=b**2/r/t*np.sqrt(1-v**2) #A value to read off k from the graph in SMAD
+print(f'Read off graph, r/t: {GREEN}{r/t}{RESET}, x axis: {GREEN}{val1}{RESET}')
 k=4
 Mod_buck=k*np.pi**2*E/(12*(1-v**2))*(t/b)**2
 print('Required buckling strength: ',Req_buck,' Pa')
 print('Model buckling strength: ',Mod_buck,' Pa')
 print('Distance between stringers: ',b*100,' cm')
+print('Buckling ratio: ',Mod_buck/Req_buck)
 if Mod_buck>Req_buck:
     print(f"{GREEN}Model can withstand buckling{RESET}")
 else:
     print(f"{RED}Buckles{RESET}")
-# x=np.linspace(0.0001,0.006,100)
-# y=0.6*E*x/r*(1-0.901*(1-np.exp(-1/16*np.sqrt(r/x))))
-# for i in range (len(x)):
-#         print(x[i],y[i]/Req_buck)
-struc_m=t*d*np.pi*l*rho + ts*ls*l*n*rho
+
+struc_m=t*d*np.pi*l*rho + ts*ls*l*n*rho #Calculating the total structural mass
 print('Mass of load bearing structure: ',struc_m,' kg')
-print(I_req)
-I_skin=np.pi*r**3*0.001
-print(np.pi*r**2*l)
+
+#Stringer and skin total MOI calculation
+I_skin=np.pi*r**3*t #Skin MOI
+angle=2*np.pi/n #angle between stringers
+lm=[]
+lm.extend([0,0,r,r])
+for i in range (1,int(n/4)):
+    d=float(np.sin(i*angle)*r)
+    lm.extend([d,d,d,d]) #this becomes a list of distances of each stringer from the center axis
+I_str=np.sum(np.square(lm)*ls*ts) #Stringer MOI, Only parallel axis, inherent MOI ignored
+I_total=I_str+I_skin
+print('Total MOI: ',I_total,'m4')
+print('Required MOI ',I_req,'m4')
+f_lat_mod=0.56*np.sqrt(E*I_total/(m*l**3))
+f_ax_mod=0.25*np.sqrt(A*E/(m*l))
+print('Axial natural frequency: ',f_ax_mod,'Hz')
+print('Lateral natural frequency: ',f_lat_mod,'Hz')
+print('Total volume',np.pi*r**2*l,'m3')
